fca7e2591f7b84104320980b0afd75c060bb340d
afratetlay <afratetlay@gmail.com>
2023-05-24 16:34:34 +0200

    working on the useEffect cleanup function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("checking");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      console.log("Cleanup");
+      clearTimeout(identifier);
+    };
   }, [setFormIsValid, enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
